e744ad4811b8459f74a4a8e42dba95b24fdd707d
Curtis Hodak <curtis@Curtiss-MacBook-Pro.local>
2025-06-14 19:28:40 -0400

    This actually works

--- main.tf
+++ main.tf
@@ -1,304 +1,360 @@
-# resource "aws_vpc" "eks_vpc" {
-#   cidr_block = "172.20.0.0/16"
-#   tags = {
-#     Name = "eks-vpc"
-#   }
-# }
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "172.20.0.0/16"
+  tags = {
+    Name = "eks-vpc"
+  }
+}
 
-# resource "aws_subnet" "public_eks_subnet" {
-#   count                   = 2
-#   vpc_id                  = aws_vpc.eks_vpc.id
-#   cidr_block              = element(["172.20.1.0/24", "172.20.2.0/24"], count.index)
-#   availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "eks-public-subnet-${count.index}"
-#   }
-# }
+resource "aws_subnet" "public_eks_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = element(["172.20.1.0/24", "172.20.2.0/24"], count.index)
+  availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-public-subnet-${count.index}"
+  }
+}
 
-# resource "aws_subnet" "private_eks_subnet" {
-#   count                   = 2
-#   vpc_id                  = aws_vpc.eks_vpc.id
-#   cidr_block              = element(["172.20.3.0/24", "172.20.4.0/24"], count.index)
-#   availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
-#   map_public_ip_on_launch = true
-#   tags = {
-#     Name = "eks-private-subnet-${count.index}"
-#   }
-# }
+resource "aws_subnet" "private_eks_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = element(["172.20.3.0/24", "172.20.4.0/24"], count.index)
+  availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-private-subnet-${count.index}"
+  }
+}
 
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.eks_vpc.id
-#   tags = {
-#     Name = "my-eks-cluster"
-#   }
-# }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "my-eks-cluster"
+  }
+}
 
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.eks_vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-#   tags = {
-#     Name = "my-eks-cluster-public"
-#   }
-# }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "my-eks-cluster-public"
+  }
+}
 
-# resource "aws_route_table_association" "public" {
-#   count          = 2
-#   subnet_id      = element(aws_subnet.public_eks_subnet[*].id, count.index)
-#   route_table_id = aws_route_table.public.id
-# }
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = element(aws_subnet.public_eks_subnet[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
 
-# resource "aws_security_group" "eks_cluster_sg" {
-#   name        = "my-eks-cluster-eks-cluster-sg"
-#   description = "EKS cluster security group"
-#   vpc_id      = aws_vpc.eks_vpc.id
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "my-eks-cluster-eks-cluster-sg"
-#   }
-# }
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "my-eks-cluster-eks-cluster-sg"
+  description = "EKS cluster security group"
+  vpc_id      = aws_vpc.eks_vpc.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "my-eks-cluster-eks-cluster-sg"
+  }
+}
 
-# resource "aws_security_group" "eks_node_sg" {
-#   name        = "my-eks-cluster-eks-node-sg"
-#   description = "EKS worker node security group"
-#   vpc_id      = aws_vpc.eks_vpc.id
-#   ingress {
-#     from_port = 0
-#     to_port   = 0
-#     protocol  = "-1"
-#     security_groups = [
-#       aws_security_group.eks_cluster_sg.id
-#     ]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "my-eks-cluster-eks-node-sg"
-#   }
-# }
+resource "aws_security_group" "eks_node_sg" {
+  name        = "my-eks-cluster-eks-node-sg"
+  description = "EKS worker node security group"
+  vpc_id      = aws_vpc.eks_vpc.id
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    security_groups = [
+      aws_security_group.eks_cluster_sg.id
+    ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "my-eks-cluster-eks-node-sg"
+  }
+}
 
-# #IAM Roles/Policies - All of the following are related to IAM Role and Policies 
-# resource "aws_iam_role" "eks_role" {
-#   name = "eks-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "eks.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+#IAM Roles/Policies - All of the following are related to IAM Role and Policies 
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_policy" {
-#   role       = aws_iam_role.eks_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-# }
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
 
-# resource "aws_iam_role" "eks_node_group_role" {
-#   name = "eks-node-group-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_node_group_policy" {
-#   role       = aws_iam_role.eks_node_group_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-# }
+resource "aws_iam_role_policy_attachment" "eks_node_group_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-#   role       = aws_iam_role.eks_node_group_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-# }
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
-#   role       = aws_iam_role.eks_node_group_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-# }
+resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
 
-# #EKS Cluster and Node Group deployment
-# resource "aws_eks_cluster" "eks_cluster" {
-#   name     = "my-eks-cluster"
-#   role_arn = aws_iam_role.eks_role.arn
-#   vpc_config {
-#     subnet_ids         = aws_subnet.private_eks_subnet[*].id
-#     security_group_ids = [aws_security_group.eks_cluster_sg.id]
-#   }
-#   access_config {
-#     authentication_mode = "API_AND_CONFIG_MAP"
-#   }
+#EKS Cluster and Node Group deployment
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+  vpc_config {
+    subnet_ids         = aws_subnet.private_eks_subnet[*].id
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 
-#   depends_on = [aws_iam_role_policy_attachment.eks_policy]
-# }
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+}
 
-# resource "aws_eks_node_group" "eks_node_group" {
-#   cluster_name    = aws_eks_cluster.eks_cluster.name
-#   node_group_name = "my-node-group"
-#   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-#   subnet_ids      = aws_subnet.private_eks_subnet[*].id
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 2
-#     min_size     = 1
-#   }
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = aws_subnet.public_eks_subnet[*].id
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
 
-#   instance_types = ["t3.medium"]
-#   remote_access {
-#     ec2_ssh_key = "personal" # Replace with your key pair name
-#   }
+  instance_types = ["t3.medium"]
+  remote_access {
+    ec2_ssh_key = "personal" # Replace with your key pair name
+  }
 
-#   update_config {
-#     max_unavailable = 1
-#   }
+  update_config {
+    max_unavailable = 1
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.eks_node_group_policy,
-#     aws_iam_role_policy_attachment.eks_cni_policy,
-#     aws_iam_role_policy_attachment.eks_ecr_policy
-#   ]
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_group_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_ecr_policy,
+    aws_eks_access_entry.curtis,
+    aws_eks_access_entry.spacelift,
+    aws_eks_access_policy_association.chclusteradmin,
+    aws_eks_access_policy_association.chnode,
+    aws_eks_access_policy_association.curtis,
+    aws_eks_access_policy_association.spacelift
+  ]
 
-# }
+}
 
-# data "aws_eks_cluster" "eks_cluster" {
-#   name = aws_eks_cluster.eks_cluster.name
-# }
+data "aws_eks_cluster" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
 
-# data "aws_eks_cluster_auth" "eks_cluster" {
-#   name = aws_eks_cluster.eks_cluster.name
-# }
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
 
-# #Kubernetes provider for Terraform to connect with AWS EKS Cluster
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#   token                  = data.aws_eks_cluster_auth.eks_cluster.token
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-# }
+#Kubernetes provider for Terraform to connect with AWS EKS Cluster
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+}
 
-# #Kubernetes resources in Terraform
-# resource "kubernetes_namespace" "terraform-k8s" {
-#   metadata {
-#     name = "terraform-k8s"
-#   }
-# }
+#Kubernetes resources in Terraform
+resource "kubernetes_namespace" "terraform-k8s" {
+  metadata {
+    name = "terraform-k8s"
+  }
 
-# resource "kubernetes_deployment" "nginx" {
-#   metadata {
-#     name      = "nginx"
-#     namespace = kubernetes_namespace.terraform-k8s.metadata[0].name
-#   }
+  depends_on = [aws_iam_role_policy_attachment.eks_node_group_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_ecr_policy,
+    aws_eks_access_entry.curtis,
+    aws_eks_access_entry.spacelift,
+    aws_eks_access_policy_association.chclusteradmin,
+    aws_eks_access_policy_association.chnode,
+    aws_eks_access_policy_association.curtis,
+    aws_eks_access_policy_association.sleks,
+    aws_eks_access_policy_association.slnode,
+    aws_eks_access_policy_association.slcluster
+    ]
+}
 
-#   spec {
-#     replicas = 1
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.terraform-k8s.metadata[0].name
+  }
 
-#     selector {
-#       match_labels = {
-#         app = "nginx"
-#       }
-#     }
+  spec {
+    replicas = 1
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = "nginx"
-#         }
-#       }
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
 
-#       spec {
-#         container {
-#           name  = "nginx"
-#           image = "nginx:1.21.6"
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
 
-#           port {
-#             container_port = 80
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.21.6"
 
-# resource "kubernetes_service" "nginx" {
-#   metadata {
-#     name      = "nginx"
-#     namespace = kubernetes_namespace.terraform-k8s.metadata[0].name
-#   }
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
 
-#   spec {
-#     selector = {
-#       app = kubernetes_deployment.nginx.spec[0].template[0].metadata[0].labels.app
-#     }
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.terraform-k8s.metadata[0].name
+  }
 
-#     port {
-#       port        = 80
-#       target_port = 80
-#     }
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.spec[0].template[0].metadata[0].labels.app
+    }
 
-#     type = "LoadBalancer"
-#   }
-# }
+    port {
+      port        = 80
+      target_port = 80
+    }
 
-# #Output Load Balancer IP to access from browser
-# output "nginx_load_balancer_ip" {
-#   value = kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
-# }
+    type = "LoadBalancer"
+  }
+}
 
+#Output Load Balancer IP to access from browser
+output "nginx_load_balancer_ip" {
+  value = kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
+}
 
-# resource "aws_eks_access_entry" "spacelift" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   principal_arn = "arn:aws:iam::748229905537:role/spacelift"
-#   kubernetes_groups = ["system:masters"]
-#   type          = "STANDARD"
-# }
+resource "aws_eks_access_entry" "curtis" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
+  type          = "STANDARD"
+}
 
-# resource "aws_eks_access_policy_association" "spacelift" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-#   principal_arn = "arn:aws:iam::748229905537:role/spacelift"
+resource "aws_eks_access_policy_association" "curtis" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
 
-#   access_scope {
-#     type = "cluster"
-#   }
-# }
+  access_scope {
+    type = "cluster"
+  }
+}
 
-# resource "aws_eks_access_entry" "curtis" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
-#   kubernetes_groups = ["system:masters"]
-#   type          = "STANDARD"
-# }
+resource "aws_eks_access_policy_association" "chnode" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAutoNodePolicy"
+  principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
 
-# resource "aws_eks_access_policy_association" "curis" {
-#   cluster_name  = aws_eks_cluster.eks_cluster.name
-#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
-#   principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
+  access_scope {
+    type = "cluster"
+  }
+}
 
-#   access_scope {
-#     type = "cluster"
-#   }
-# }
+resource "aws_eks_access_policy_association" "chclusteradmin" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::748229905537:user/curtisdev1"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_eks_access_entry" "spacelift" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  principal_arn = "arn:aws:iam::748229905537:role/spacelift"
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "sleks" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  principal_arn = "arn:aws:iam::748229905537:role/spacelift"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+  resource "aws_eks_access_policy_association" "slnode" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAutoNodePolicy"
+  principal_arn = "arn:aws:iam::748229905537:role/spacelift"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_eks_access_policy_association" "slcluster" {
+  cluster_name  = aws_eks_cluster.eks_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::748229905537:role/spacelift"
+
+  access_scope {
+    type = "cluster"
+  }
+}
